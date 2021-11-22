75024aeaf94fc2612be321953defb03f1bd2d37e
torvic <130111@unsaac.edu.pe>
2021-11-22 10:32:51 -0500

    Fix: Add accent to the paragraph

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
 		<div>
 			<h1>Mi primera aplicación</h1>
-			<p>Esta es mi primera aplicación en React, esta padrísimo!</p>
+			<p>Esta es mi primera aplicación en React, está padrísimo!</p>
 		</div>
   );
 }
